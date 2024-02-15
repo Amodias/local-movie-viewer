63dd0a49851c5a12e90ad3e0f93f2c1bf766a9cb
Belgacem Saïd Amokrane <101249638+Amodias@users.noreply.github.com>
2024-02-15 09:30:58 +0100

    add Qr code to read local ipadress

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../node_modules/video-react/dist/video-react.css";
 import { Player } from "video-react";
+import QRCode from "react-qr-code";
 
 function App() {
+  const [serverIpAddress, setServerIpAddress] = useState("");
+
+  useEffect(() => {
+    const hostname = window.location.hostname;
+    const port = window.location.port;
+    setServerIpAddress(`http://${hostname}:${port}`);
+  });
+
   return (
     <div className="container gap-5 p-5">
       <header className="p-5">
@@ -14,12 +23,15 @@ function App() {
           <div className="col-span-1"></div>
         </div>
       </header>
-      <div className="grid justify-items-center">
-        <div className="w-1/2 h-1/2 ">
+      <div className="grid justify-items-center grid-cols-3">
+        <div className="w-1/2 h-1/2 col-span-2">
           <Player
             playsInline
             src="/movies/sample/pexels-thirdman-5538262.mp4"
           />
+        </div>
+        <div className="col-span-1">
+          <QRCode value={serverIpAddress} />
         </div>
       </div>
     </div>
