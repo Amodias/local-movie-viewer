3d3fb9bc6fd1497fea8a33343f2dc511525fad11
amodias <saidamokranebelgacem@gmail.com>
2024-02-16 13:39:38 +0100

    🗃️ refactor app and set router and create different pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "../node_modules/video-react/dist/video-react.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Player } from "video-react";
 import QRCode from "react-qr-code";
+import { Home } from "./pages/Home";
 
 function App() {
   const [serverIpAddress, setServerIpAddress] = useState("");
@@ -13,28 +15,12 @@ function App() {
   });
 
   return (
-    <div className="container gap-5 p-5">
-      <header className="p-5">
-        <div className="grid grid-cols-5">
-          <div className="col-span-1"></div>
-          <div className="col-span-3">
-            <input type="text" placeholder="search here" />
-          </div>
-          <div className="col-span-1"></div>
-        </div>
-      </header>
-      <div className="grid justify-items-center grid-cols-3">
-        <div className="w-1/2 h-1/2 col-span-2">
-          <Player
-            playsInline
-            src="/movies/sample/pexels-thirdman-5538262.mp4"
-          />
-        </div>
-        <div className="col-span-1">
-          <QRCode value={serverIpAddress} />
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="users/:id" element={<Users />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="container bg-current">hey</div>;
+};
+
+export { Home };
