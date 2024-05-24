ee62928a8283bc199a730f4f65e564d345d7b087
amodias <saidamokranebelgacem@gmail.com>
2024-05-24 10:39:26 +0100

    💥 include server side

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use("/media", express.static(path.join(__dirname, "../media")));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
